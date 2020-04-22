8b711c5a1f20f3403c3f66ec4d94b8d9c9d9d2f2
DokimiCU <DokimiCU@gmail.com>
2020-04-22 12:55:58 +1200

    more quotes

--- mods/bed_rest/functions.lua
+++ mods/bed_rest/functions.lua
@@ -9,8 +9,6 @@ local pi = math.pi
 
 
 
-
-
 ----------------------------------------------------
 --Break taker
 --
@@ -63,7 +61,15 @@ local quote_list = {
 	"'There is scarcely any passion without struggle.'\n- Albert Camus",
 	"'O snail\nClimb Mount Fuji\nBut slowly, slowly!'\n-  Kobayashi Issa",
 	"'Not all those who wander are lost.'\n- Tolkien",
-	"'I've never been lost, but I was mighty turned around for three days once.'\n- Daniel Boone"
+	"'I've never been lost, but I was mighty turned around for three days once.'\n- Daniel Boone",
+	"'Dripping water hollows out stone, not through force but through persistence.'\n- Ovid",
+	"'I have not failed. I've just found 10,000 ways that won't work.'\n- Thomas Edison",
+	"'I'm not afraid of death; I just don't want to be there when it happens.'\n- Woody Allen",
+	"'Reality continues to ruin my life.'\n- Bill Watterson",
+	"'Give a man a fire and he's warm for a day, but set fire to him and he's warm for the rest of his life.'\n- Terry Pratchett",
+	"'There's a fine line between genius and insanity. I have erased this line.'\n- Oscar Levant",
+	"'Let's think the unthinkable, let's do the undoable.\nLet us prepare to grapple with the ineffable itself, and see if we may not eff it after all.'\n- Douglas Adams",
+	"'When we are tired, we are attacked by ideas we conquered long ago.'\n- Friedrich Nietzsche"
 
 
 
@@ -263,3 +269,4 @@ minetest.register_on_dieplayer(function(player)
 	player_api.set_animation(player, "stand")
 
 end)
+
