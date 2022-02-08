63689bb772be2944f48071fa838787b703b4f9c6
Jeremy <jeremylshannon@gmail.com>
2022-02-07 21:33:58 -0800

    Remove exile letter from a player's inventory on suicide

--- mods/lore/suicide.lua
+++ mods/lore/suicide.lua
@@ -3,15 +3,27 @@
 
 --Local table to store pending confirmations.  
 local chat_confirm = {};
+local timestamp = {}
 
-
+local function killplayer(name)
+   local player=minetest.get_player_by_name(name)
+   local inv = player:get_inventory()
+   if inv:is_empty("cloths") then
+      -- no clothes, try to pull an exile letter and see if main's empty then
+      local tmp = inv:remove_item("main", ItemStack("lore:exile_letter 1"))
+      if not inv:is_empty("main") then
+	 inv:add_item("main", tmp) -- he's got other stuff, put it back
+      end
+   end
+   player:set_hp(0)
+end
 
 local function suicide_confirm (name, message)
 	if (chat_confirm[name] == 'suicide') then
-		if message == 'Yes' then
-			local player=minetest.get_player_by_name(name)
+		if message == 'Yes' or message == "yes" then
 			minetest.chat_send_all(name .. " succumbed to despair and gave in to their fate.")
-			player:set_hp(0);
+			timestamp[name] = minetest.get_gametime()
+			killplayer(name)
 		else
 			minetest.chat_send_player(name, "You've come to your senses and decided to keep trying")
 		end
@@ -21,10 +33,16 @@ local function suicide_confirm (name, message)
 	return false -- let other modules see it.
 end
 
-
-local function suicide (name, param) 
-	minetest.chat_send_player(name, "Are you sure?  Reply with: Yes")
-	chat_confirm[name]="suicide";
+local function suicide (name, param)
+	local nowtime = minetest.get_gametime()
+	if timestamp[name] and ( timestamp[name] +300 ) > nowtime then
+	   minetest.chat_send_player(name, "You can't use this command more than once per 5 minutes.")
+	   return
+	else
+	   timestamp[name] = nil
+	   minetest.chat_send_player(name, "Are you sure?  Reply with: Yes")
+	   chat_confirm[name]="suicide";
+	end
 end
 
 minetest.register_chatcommand("suicide",{
