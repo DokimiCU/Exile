f2e02066875e990c267d62265bebdf8c636543c6
Jeremy <jeremylshannon@gmail.com>
2022-03-17 00:52:16 -0700

    Update warnings about settings

--- mods/minimal/settingswarn.lua
+++ mods/minimal/settingswarn.lua
@@ -8,11 +8,12 @@ if timespeed and ( timespeed ~= "60" and
 		   timespeed ~= "96") then
    minetest.log("warning", "time_speed should be set to 60, 72, or 96 for intended balance")
 end
-if minetest.get_mapgen_setting("mg_name") ~= "valleys" then
-   minetest.log("error", "Mapgens other than valleys are unsupported and can break the game!")
+if minetest.get_mapgen_setting("mg_name") ~= "valleys" and
+   minetest.get_mapgen_setting("mg_name") ~= "carpathian" then
+   minetest.log("error", "Mapgens other than valleys or carpathian are unsupported and can break the game!")
 end
 
 local HUS = tonumber(minetest.settings:get("exile_hud_update"))
 if HUS < 1 and not minetest.is_singleplayer() then
-   minetest.log("warning", "It is recommended to set hud update speed to 1 second or longer on multiplayer servers")
+   minetest.log("warning", "It is recommended to set hud update speed to 1 second on multiplayer servers")
 end
