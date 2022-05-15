361764ab78ecd091e01941da13ad5877bcc93090
Jeremy <jeremylshannon@gmail.com>
2022-05-15 16:25:45 -0700

    Revert "Save info on bed type and direction" We're not going to need that, actually
    This reverts commit 8f9ba297779dd121c02b21f5d38216d0dfeef8f2.

--- mods/bed_rest/init.lua
+++ mods/bed_rest/init.lua
@@ -4,13 +4,10 @@
 ------------------------------------
 --
 bed_rest = {}
-bed_rest.player = {} -- player_name = 1 if they're in bed, 2 if bed is stored
-bed_rest.bed_position = {} -- position of the bed
-bed_rest.pos = {} -- position of the player
-bed_rest.level = {} -- height of the bed
--- If a bed is stored due to inactivity:
-bed_rest.type = {} -- itemname for the bed
-bed_rest.dir = {} -- param2 of the bed
+bed_rest.player = {}
+bed_rest.bed_position = {}
+bed_rest.pos = {}
+bed_rest.level = {}
 
 bed_rest.session_start = {}
 bed_rest.session_limit = {}
@@ -20,7 +17,6 @@ bed_rest.session_limit = {}
 local modpath = minetest.get_modpath("bed_rest")
 
 -- Load files
-
 
 dofile(modpath .. "/functions.lua")
 dofile(modpath .. "/api.lua")
@@ -32,6 +28,4 @@ if temp then
    bed_rest.player = temp["player"]
    bed_rest.pos = temp["pos"]
    bed_rest.bed_position = temp["bed_position"]
-   bed_rest.type = temp["type"]
-   bed_rest.dir = temp["dir"]
 end
