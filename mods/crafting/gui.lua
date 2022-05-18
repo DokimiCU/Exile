19d3a16c89357fa6e0441b65de8463b8b35cae58
Jeremy <jeremylshannon@gmail.com>
2022-05-17 22:31:16 -0700

    Add escapes for lua magic characters, and remove 'em all

--- mods/crafting/gui.lua
+++ mods/crafting/gui.lua
@@ -160,7 +160,10 @@ function crafting.make_result_selector(player, type, level, size, context)
 end
 
 local function sanitize(badstring)
-   local disallowed = { "\\", "{", "}" }
+   local disallowed = { "\\", "{", "}",
+			--lua magic characters
+			"%(", "%)", "%[", "%]", "%.", "%$",
+			"%^", "%%", "%+", "%-", "%*", "%?"  }
    badstring:trim():lower()
    for i in ipairs(disallowed) do
       badstring = badstring:gsub(disallowed[i],"")
