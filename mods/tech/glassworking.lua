fbaecce283fa1fdb81b5d9d454228a50a1f496b9
Jeremy <jeremylshannon@gmail.com>
2022-06-03 11:46:46 -0700

    Restore temp for glassmaking products so ingots stay hot

--- mods/tech/glassworking.lua
+++ mods/tech/glassworking.lua
@@ -72,6 +72,9 @@ local function roast(pos, selfname, name, heat)
 	if roasting <= 0 then
 		--finished firing
     minetest.set_node(pos, {name = name})
+    if minetest.get_item_group(name,"heatable") > 0 then
+       meta:set_float("temp", temp)
+    end
     minetest.check_for_falling(pos)
     return false
   elseif temp < fire_temp then
