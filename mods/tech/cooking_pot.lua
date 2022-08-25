2796fdeec7e40dbf9f125fac6d568c261695db60
Ceeee <marius.romanus@gmx.de>
2022-08-25 23:10:26 +0200

    Soup icon (#195)
    * added new icon
    * modified soup item in cooking pot to incorporate new graphics

--- mods/tech/cooking_pot.lua
+++ mods/tech/cooking_pot.lua
@@ -64,7 +64,7 @@ local pot_formspec = "size[8,4.1]"..
 
 minetest.register_craftitem("tech:soup", {
 	description = S("Soup"),
-	inventory_image = "tech_vegetable_oil.png",
+	inventory_image = "tech_soup.png",
 	stack_max = minimal.stack_max_medium,
 	on_use = function(itemstack, user, pointed_thing)
 	   return exile_eatdrink_playermade(itemstack, user)
